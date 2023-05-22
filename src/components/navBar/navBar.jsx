51b9e7c1fad9046e302a8766cc900d37c19a1b79
AazimAnish <aazimanish1@gmail.com>
2023-05-22 14:40:16 +0530

    added navbar

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -3,11 +3,12 @@ import "./navBar.css"
 export function Navbar() {
     return(
         <nav className="navbar">
+            <p> &lt; AazimAnish /&gt;</p>
             <ul className="navbar-list">
-                <li><a href="#contact-me">_contact-me</a></li>
-                <li><a href="#about-me">_about-me</a></li>
-                <li><a href="#projects">_projects</a></li>
-                <li><a href="#home">Hello</a></li>
+                <li><a className="navbotton" href="#home">_Hello</a></li>
+                <li><a className="navbotton" href="#projects">_projects</a></li>
+                <li><a className="navbotton" href="#about-me">_about-me</a></li>
+                <li><a className="navbotton" href="#contact-me">_contact-me</a></li>
             </ul>
             <hr className="line" />
         </nav>
