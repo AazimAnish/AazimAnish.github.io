455ef239d47c62747fa194eb8f498b95caa5d4ba
AazimAnish <aazimanish1@gmail.com>
2023-05-23 20:33:32 +0530

    homePage text

--- src/components/homePage/homePage.jsx
+++ src/components/homePage/homePage.jsx
@@ -6,7 +6,18 @@ const Home = () => {
   return (
     <div className="container">
       <Navbar />
+      <div className="main">
+        <h3>Hi all. I am,</h3>
+        <h1>Aazim Anish</h1>
+        <h2>&gt; Tech Enthusiast</h2>
+        </div>
+        <div className='commenttext'>
+        <p className='line1'>// welcome to my portfolio, press z</p>
+        <p className='line2'>// you can also see my projects on my Github page</p>
+        <p className='line3'>const githubLink = "<a href="https://github.com/AazimAnish">https://github.com/AazimAnish</a>"</p>
+        </div>
     </div>
+
   );
 };
 
--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -3,7 +3,7 @@ import "./navBar.css"
 export function Navbar() {
     return(
         <nav className="navbar">
-            <p> &lt; AazimAnish /&gt;</p>
+            <p className="name"> &lt; AazimAnish /&gt;</p>
             <ul className="navbar-list">
                 <li><a className="navbotton" href="#home">_Hello</a></li>
                 <li><a className="navbotton" href="#projects">_projects</a></li>
