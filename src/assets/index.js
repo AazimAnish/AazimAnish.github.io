0b773c561f4acea5655e5de7292059067134c283
AazimAnish <aazimanish1@gmail.com>
2023-08-15 20:04:01 +0530

    App.js outline

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import { MoonIcon } from "./MoonIcon";
+import ProjectSample from "./ProjectSample.png"
+import { SunIcon } from "./SunIcon";
+
+export {
+    ProjectSample,
+    SunIcon,
+    MoonIcon
+};
